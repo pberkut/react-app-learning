9cc844d82f051c2b7b82da6a39fdd55267e17ac1
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-02-22 08:33:03 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,22 @@
 // import './App.css';
 
-import { ColorPicker } from './ColorPicker';
-import { Dropdown } from './Dropdown';
+// import { ColorPicker } from './ColorPicker';
+// import { Dropdown } from './Dropdown';
 import { GlobalStyles } from './GlobalStyles';
 import { RecipeList } from './RecipeList';
 import recipes from './RecipeList/recipes.json';
 import { Wrapper } from './Wrapper/Wrapper';
 // import { DisplayOutput } from './DisplayOutput/DisplayOutput';
 // import { Button } from './Button/Button';
-import { Counter } from './Counter';
-import { TodoListApp } from './TodoListApp';
+// import { Counter } from './Counter';
+// import { TodoListApp } from './TodoListApp';
 
 // import Section from './components/Section';
 // import users from './data/users.json';j
 // import UserProfileList from './components/UserProfileList';
 
 // * import data JSON
-import optionsColorPicker from './ColorPicker/ColorPickerData.json';
+// import optionsColorPicker from './ColorPicker/ColorPickerData.json';
 
 export default function App() {
   return (
