28bbe612416ff8bfb4c06805b6a1a7f847770217
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-03-02 19:17:34 +0200

    Add State Machine Pattern

--- src/components/PokemonApp/components/PokemonInfo/PokemonInfo.jsx
+++ src/components/PokemonApp/components/PokemonInfo/PokemonInfo.jsx
@@ -1,22 +1,67 @@
 import { Component } from 'react';
 
+/*
+
+Паттерн state machine pattern - Стейт Машина
+У нас будет 4 состояния:
+ 'idle' - простой, стоит на месте
+ 'pending' - ожидается выполнение
+ 'resolved' - Выполнилось с результатом, это хорошо
+ 'rejected' - отклонено, результата нет
+
+*/
 export class PokemonInfo extends Component {
+  // Вариант № 1
+  //   state = {
+  //     pokemon: null,
+  //     loading: false,
+  //     error: null,
+  //     status: 'idle',
+  //   };
+
   state = {
     pokemon: null,
-    loading: false,
     error: null,
+    status: 'idle',
   };
+
+  // Вариант № 1
+  //   componentDidUpdate(prevProps, prevSate) {
+  //     const prevName = prevProps.pokemonName;
+  //     const nextName = this.props.pokemonName;
+
+  //     if (prevName !== nextName) {
+  //       console.log('Change props');
+  //       console.log('prevName: ', prevName);
+  //       console.log('nextName ', nextName);
+
+  //       this.setState({ loading: true, pokemon: null });
+  //       fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
+  //         .then(response => {
+  //           if (response.ok) {
+  //             return response.json();
+  //           }
+
+  //           return Promise.reject(
+  //             new Error(`No Pokemon witt this name ${nextName} `)
+  //           );
+  //         })
+  //         .then(pokemon => this.setState({ pokemon }))
+  //         .catch(error => this.setState({ error }))
+  //         .finally(() => this.setState({ loading: false }));
+  //     }
+  //   }
 
   componentDidUpdate(prevProps, prevSate) {
     const prevName = prevProps.pokemonName;
     const nextName = this.props.pokemonName;
 
     if (prevName !== nextName) {
-      console.log('Change props');
-      console.log('prevName: ', prevName);
-      console.log('nextName ', nextName);
+      //   console.log('Change props');
+      //   console.log('prevName: ', prevName);
+      //   console.log('nextName ', nextName);
 
-      this.setState({ loading: true });
+      this.setState({ status: 'pending' });
       fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
         .then(response => {
           if (response.ok) {
@@ -27,37 +72,75 @@ export class PokemonInfo extends Component {
             new Error(`No Pokemon witt this name ${nextName} `)
           );
         })
-        .then(pokemon => this.setState({ pokemon }))
-        .catch(error => this.setState({ error }))
-        .finally(() => this.setState({ loading: false }));
+        .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
+        .catch(error => this.setState({ error, status: 'rejected' }));
     }
   }
 
+  /* 
+    'idle'
+    'pending'
+    'resolved'
+    'rejected'
+*/
+
   render() {
-    const { pokemon, loading, error } = this.state;
-    const { pokemonName } = this.props;
-    return (
-      <div>
-        <h1>PokemonInfo</h1>
-        {error && (
-          <div>
-            <h1>Error! Pokemon with name - {pokemonName} undefined</h1>
-            <p>Status error: {error.message}</p>
-          </div>
-        )}
-        {loading && <div>Loading...</div>}
-        {!pokemonName && <div>Enter name pokemon</div>}
-        {pokemon && (
-          <div>
-            <p>{pokemon.name}</p>
-            <img
-              src={pokemon.sprites.other['official-artwork'].front_default}
-              alt={pokemon.name}
-              width="240"
-            />
-          </div>
-        )}
-      </div>
-    );
+    // Вариант №1
+    // const { pokemon, loading, error, status } = this.state;
+    // const { pokemonName } = this.props;
+
+    const { pokemon, error, status } = this.state;
+
+    if (status === 'idle') {
+      return <div>Enter name pokemon</div>;
+    }
+
+    if (status === 'pending') {
+      return <div>Loading...</div>;
+    }
+
+    if (status === 'rejected') {
+      return <p>Status error: {error.message}</p>;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <div>
+          <p>{pokemon.name}</p>
+          <img
+            src={pokemon.sprites.other['official-artwork'].front_default}
+            alt={pokemon.name}
+            width="240"
+          />
+        </div>
+      );
+    }
+
+    // Вариант № 1
+    // return (
+    //   <div>
+    //     <h1>PokemonInfo</h1>
+    //     {error && (
+    //       <div>
+    //         {/*  Это я использую как своё сообщение для пользователя. Это я оставил для себя для примера*/}
+    //         {/* <h1>Error! Pokemon with name - {pokemonName} undefined</h1> */}
+    //         {/* Это я использую сообщение с обработчика. Статус ошибки. Вот так обрабатывается правильно ошибка  */}
+    //         <p>Status error: {error.message}</p>
+    //       </div>
+    //     )}
+    //     {loading && <div>Loading...</div>}
+    //     {!pokemonName && <div>Enter name pokemon</div>}
+    //     {pokemon && (
+    //       <div>
+    //         <p>{pokemon.name}</p>
+    //         <img
+    //           src={pokemon.sprites.other['official-artwork'].front_default}
+    //           alt={pokemon.name}
+    //           width="240"
+    //         />
+    //       </div>
+    //     )}
+    //   </div>
+    // );
   }
 }
