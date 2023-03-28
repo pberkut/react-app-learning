6f7ac3c544245f317c33afd038c6d10b7c300a56
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-03-28 07:10:36 +0300

    update

--- src/apps/TodoReduxToolkitApp/redux/store.js
+++ src/apps/TodoReduxToolkitApp/redux/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import { tasksReducer } from './tasksSlice';
 import { filtersReducer } from './filtersSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     tasks: tasksReducer,
     filters: filtersReducer,
   },
 });
+
+export { store };
